1cf163051361c09ed974e938d98f754ad622fe56
Ruby Josh <rubyjessiej@gmail.com>
2018-07-12 14:26:14 +0530

    Update evenfind.py

--- evenfind.py
+++ evenfind.py
@@ -1,7 +1,8 @@
 num=int(input())
-if(num==0):
-	print("zero")
-elif(num%2==0):
-	print("Even")
+if(num>0):
+	if(num%2==0):
+		print("Even")
+	else:
+		print("Odd")
 else:
-	print("Odd")
+	print("invalid")
